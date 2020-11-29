b35d99a228bc1826dbb6f22257c12e178e48f27b
Arthur Novaes <arthur.novaes@enacom.com.br>
2020-11-29 14:09:15 -0300

    updating the interface of actv 10

--- query/tests/app.py
+++ query/tests/app.py
@@ -11,9 +11,13 @@ def show_index_html():
 @app.route('/send_data', methods=['POST'])
 def get_data_from_html():
     pay = request.form['pay']
+    model = request.form['model']
+    relevant_doc = request.form['relevant_doc']
     print("A consuta é: " + pay)
+    print("A modelagem é: " + model)
+    print("A arquivo relevante é: " + relevant_doc)
     return "Dado recebido. Por favor, confira o log do programa"
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
